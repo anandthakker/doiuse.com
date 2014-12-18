d267d8b00063776534e2b5db1a1b53f2f29747f3
Anand Thakker <vestibule@anandthakker.net>
2014-12-18 10:05:03 +0100

    Accept queries via URL param.
    Closes #3.

--- browser.js
+++ browser.js
@@ -1,13 +1,11 @@
 var xhr = require('xhr');
-var mustache = require('mustache');
-var fs = require('fs');
+var render = require('./lib/render');
 
 module.exports = {
   bindElement: require('./lib/bind'),
   process: processCss
 }
 
-var template = fs.readFileSync('./lib/usage.tpl.html', 'utf8');
 function processCss(options, cb) {
   xhr({
     body: JSON.stringify(options),
@@ -25,7 +23,7 @@ function processCss(options, cb) {
       .trim()
       .split('\n')
       .map(function(s) { return JSON.parse(s); })
-      .map(function(usage) { return mustache.render(template, usage); } )
+      .map(render)
       .join('')
     );
   });
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+var fs = require('fs');
 var http = require('http');
 var qs = require('querystring');
 var concat = require('concat-stream');
@@ -7,37 +8,30 @@ var styles = require('style-stream');
 var doiuse = require('doiuse/stream');
 var defaultBrowsers = require('doiuse').default;
 var ecstatic = require('ecstatic');
+var trumpet = require('trumpet');
 
-
+var render = require('./lib/render');
 var stat = ecstatic({ root: __dirname + '/public' });
 
 var server = http.createServer(function(req, res) {
   
   // Query for browser support analysis:
-  //
-  // POST { "browsers": ["ie >= 8","last 2 versions"], "url":"http://caniuse.com" }
+  // POST / { "browsers": ["ie >= 8","last 2 versions"], "url":"http://caniuse.com" }
   // or
-  // POST { "browsers": ["ie >= 8","last 2 versions"], "css":"a { transition: 1s all; }" }
+  // POST / { "browsers": ["ie >= 8","last 2 versions"], "css":"a { transition: 1s all; }" }
   //
   // Response is ld-json unsupported feature usage data, as provided by doiuse.
-  //
+  
   if (req.method == 'POST') {
     req
     .pipe(limit(1e6, function(){request.connection.destroy()}))
     .pipe(concat(function(data) {
       try {
         var body = JSON.parse(data);
+        doiuseStream(body, res)
+          .pipe(through.obj(pruneFeatureUsage))
+          .pipe(res);
         
-        if(body.browsers.trim().length === 0) body.browsers = defaultBrowsers;
-        var doi = doiuse(body.browsers, {json: true})
-        doi.pipe(through.obj(pruneFeatureUsage))
-        .pipe(res);
-        
-        if(body.url) {
-          styles({url: body.url}).pipe(doi);
-        } else if(body.css) {
-          doi.end(body.css)
-        }
       } catch(e) {
         console.error(e);
         res.statusCode = 500;
@@ -46,7 +40,20 @@ var server = http.createServer(function(req, res) {
     }));
   }
   else {
-    stat(req, res);
+    var args = qs.parse(req.url.split('?').splice(1).join('?'));
+    if(args.url) {
+      // prerender results.
+      var main = trumpet();
+      var results = main.select('.results').createWriteStream();
+      doiuseStream(args)
+        .pipe(through.obj(renderDoiuseResult))
+        .pipe(results);
+        
+      fs.createReadStream(__dirname + '/public/index.html')
+        .pipe(main)
+        .pipe(res);
+    }
+    else stat(req, res);
   }
 })
 
@@ -56,7 +63,26 @@ server.listen(port, function() {
 });
 
 
-// 
+function doiuseStream(options) {
+  if(!options.browsers || (options.browsers.trim().length === 0))
+    options.browsers = defaultBrowsers;
+
+  var doi = doiuse(options.browsers, {json: true})
+  
+  if(options.url && options.url.trim().length > 0) {
+    styles({url: options.url}).pipe(doi);
+  } else {
+    doi.end(options.css)
+  }
+  
+  return doi;
+}
+
+
+function renderDoiuseResult(usageInfo, enc, next) {
+  next(null, render(usageInfo));
+}
+
 function pruneFeatureUsage(usageInfo, enc, next) {
   if(usageInfo && usageInfo.featureData) delete usageInfo.featureData.caniuseData.stats
   next(null, JSON.stringify(usageInfo) + '\n');
