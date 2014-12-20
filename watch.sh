7f33474f2fe3ae1b261b364532910b6903ee51a8
Anand Thakker <vestibule@anandthakker.net>
2014-12-20 20:08:21 +0100

    better responsiveness

---
+++ watch.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+. ~/.nvm/nvm.sh
+nvm use 0.10
+nodemon index.js &
+nodemon -w sass/*.* node_modules/.bin/node-sass --include-path sass sass/main.scss public/main.css &
+node_modules/.bin/watchify -r ./browser.js:doiuse-browser -o public/bundle.js -v
