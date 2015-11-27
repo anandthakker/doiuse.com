39e50c293b4f07a53500183ae73f0a6b7d5e4e6a
Anand Thakker <vestibule@anandthakker.net>
2015-11-27 08:23:53 -0500

    Fix lint errors

--- browser.js
+++ browser.js
@@ -1,13 +1,12 @@
-
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var xhr = require('xhr');
-var mustache = require('mustache');
+var fs = require('fs')
+var url = require('url')
+var qs = require('querystring')
+var xhr = require('xhr')
+var mustache = require('mustache')
 
 // syntax highlighting
-require('./public/vendor/prism.css');
-var prism = require('./public/vendor/prism.js');
+require('./public/vendor/prism.css')
+var prism = require('./public/vendor/prism.js')
 
 var templates = {
   results: fs.readFileSync(__dirname + '/templates/results.html', 'utf8'),
@@ -16,16 +15,16 @@ var templates = {
 }
 
 // shorthand
-var $ = document.querySelector.bind(document);
-$.remove = function(el) {
-  if(typeof el === 'string') el = $(el);
-  if(!el) return;
-  el.parentNode.removeChild(el);
+var $ = document.querySelector.bind(document)
+$.remove = function (el) {
+  if (typeof el === 'string') el = $(el)
+  if (!el) return
+  el.parentNode.removeChild(el)
 }
 
 // elements
-var button = $('button');
-var loading = $('.loading');
+var button = $('button')
+var loading = $('.loading')
 
 var input = {
   url: $('[name="url"]'),
@@ -34,115 +33,114 @@ var input = {
 }
 
 // onload
-document.addEventListener('DOMContentLoaded', function() {
-  $('.fouc').classList.remove('fouc');
+document.addEventListener('DOMContentLoaded', function () {
+  $('.fouc').classList.remove('fouc')
 
   // watch for changes in the input fields.
-  [
+  ;[
     input.url,
     input.css,
     input.browsers
-  ].forEach(function(el) {
-    el.addEventListener('keyup', validate);
-    el.addEventListener('blur', validate);
+  ].forEach(function (el) {
+    el.addEventListener('keyup', validate)
+    el.addEventListener('blur', validate)
   })
 
   // handle click & enter key field
-  button.addEventListener('click', function(e) { fetch(); });
-  [
+  button.addEventListener('click', function (e) { fetch(); })
+  ;[
     input.url,
     input.browsers
-  ].forEach(function(el) {
-    el.addEventListener('keyup', function(e) {
-      if (e.keyCode === 13) fetch();
+  ].forEach(function (el) {
+    el.addEventListener('keyup', function (e) {
+      if (e.keyCode === 13) fetch()
     })
-  });
-  
-  var args = qs.parse(url.parse(window.location.href).query);
-  if(args.url || args.css) {
-    button.removeAttribute('disabled');
-    fetch(args);
+  })
+
+  var args = qs.parse(url.parse(window.location.href).query)
+  if (args.url || args.css) {
+    button.removeAttribute('disabled')
+    fetch(args)
   }
 
-  function validate() {
+  function validate () {
     var urlvalue = input.url.value.trim(),
-      cssvalue = input.css.value.trim();
+      cssvalue = input.css.value.trim()
     if (/\./.test(urlvalue) ? !cssvalue.length : cssvalue.length) // xor
-      button.removeAttribute('disabled');
+      button.removeAttribute('disabled')
     else
-      button.setAttribute('disabled', true);
+      button.setAttribute('disabled', true)
   }
 
-  function fetch(args) {
-    if (button.getAttribute('disabled')) return;
+  function fetch (args) {
+    if (button.getAttribute('disabled')) return
 
-    loading.classList.add('show');
-    $.remove('.error');
-    $.remove('.results');
-    $.remove('.nolint');
+    loading.classList.add('show')
+    $.remove('.error')
+    $.remove('.results')
+    $.remove('.nolint')
 
-    if((input.url.value || '').trim().length > 0
-    && !/^http/.test(input.url.value)) {
-      input.url.value = input.url.value.replace(/^|^.*:\/\//, 'http://');
+    if ((input.url.value || '').trim().length > 0
+      && !/^http/.test(input.url.value)) {
+      input.url.value = input.url.value.replace(/^|^.*:\/\//, 'http://')
     }
-    if(!args) {
-      args = {};
-      for(k in input) args[k] = input[k].value;
+    if (!args) {
+      args = {}
+      for (k in input) args[k] = input[k].value
     }
-    
+
     xhr({
       body: JSON.stringify(args),
       method: 'POST',
       uri: '/',
       headers: { 'Content-Type': 'application/json' }
     },
-    function(err, resp, body) {
-      try {
-        if (!(resp.statusCode >= 200 && resp.statusCode < 400))
-          throw new Error('The server responded with a bad status: '+resp.statusCode);
-        body = JSON.parse(body);
-      }
-      catch(e) { err = e; }
-      update(err, body || resp);
-    });
+      function (err, resp, body) {
+        try {
+          if (!(resp.statusCode >= 200 && resp.statusCode < 400))
+            throw new Error('The server responded with a bad status: ' + resp.statusCode)
+          body = JSON.parse(body)
+        } catch(e) { err = e; }
+        update(err, body || resp)
+      })
   }
-    
 
-  function update(err, response, skipHistory) {
-    loading.classList.remove('show');
-    
+
+  function update (err, response, skipHistory) {
+    loading.classList.remove('show')
+
     if (err) {
       var errorMarkup = mustache.render(templates.error, {message: err.toString(), error: err })
       loading.insertAdjacentHTML('beforebegin', errorMarkup)
     }
-    else if((response.usages || []).length > 0) {
-      response.usages.forEach(function(usage) {
-        usage.count = response.counts[usage.feature];
+    else if ((response.usages || []).length > 0) {
+      response.usages.forEach(function (usage) {
+        usage.count = response.counts[usage.feature]
       })
-      loading.insertAdjacentHTML('beforebegin', mustache.render(templates.results, response));
-      prism.highlightAll();
+      loading.insertAdjacentHTML('beforebegin', mustache.render(templates.results, response))
+      prism.highlightAll()
     }
-    else if(response.usages && response.usages.length === 0) {
-      var nolint = mustache.render(templates.nolint, response);
-      $('header').insertAdjacentHTML('beforeend', nolint);
+    else if (response.usages && response.usages.length === 0) {
+      var nolint = mustache.render(templates.nolint, response)
+      $('header').insertAdjacentHTML('beforeend', nolint)
     }
-    
-    if(response) {
+
+    if (response) {
       // populate input fields with the args that were used for this query.
-      for(k in response.args)
-        if(input[k]) input[k].value = response.args[k];
-        
-      var query = qs.stringify(response.args);
+      for (k in response.args)
+        if (input[k]) input[k].value = response.args[k]
+
+      var query = qs.stringify(response.args)
       if (!skipHistory && query.length > 0 && query.length < 1e6) {
-        window.history.pushState(response, '', '/?' + query);
-        var json;
-        if(json = $('#json-link')) json.setAttribute('href', '/api?' + query);
+        window.history.pushState(response, '', '/?' + query)
+        var json
+        if (json = $('#json-link')) json.setAttribute('href', '/api?' + query)
       }
     }
   }
 
-  window.addEventListener('popstate', function(event) {
-    update(null, event.state);
-  });
+  window.addEventListener('popstate', function (event) {
+    update(null, event.state)
+  })
 
-}); // DOMContentLoaded
+}) // DOMContentLoaded
--- index.js
+++ index.js
@@ -1,67 +1,59 @@
+var fs = require('fs')
+var http = require('http')
+var qs = require('querystring')
+var url = require('url')
+var concat = require('concat-stream')
+var trumpet = require('trumpet')
+var ecstatic = require('ecstatic')
 
-var fs = require('fs');
-var http = require('http');
-var qs = require('querystring');
-var url = require('url');
-var concat = require('concat-stream');
-var through = require('through2');
-var trumpet = require('trumpet');
-var ecstatic = require('ecstatic');
+var debug = require('debug')('doiuse:server')
 
-var debug = require('debug')('doiuse:server');
-var logmem = require('./lib/logmem');
+var limit = require('./lib/limit')
+var cssFeatures = require('./lib/css-features')
 
-var limit = require('./lib/limit'),
-    cssFeatures = require('./lib/css-features');  
+var stat = ecstatic({root: __dirname + '/public', gzip: true})
 
-
-var stat = ecstatic({root: __dirname + '/public',gzip: true});
-
-var server = http.createServer(function(req, res) {
-  logmem();
-  debug(req.method, req.url);
+var server = http.createServer(function (req, res) {
+  debug(req.method, req.url)
 
   // POST /
-  // arguments (either query string or POST body) should have either `url` xor 
+  // arguments (either query string or POST body) should have either `url` xor
   // `css` property, and optional `browsers`.
   // response is {args: {given args}, usages: [usages of queried features], count:{feature:count}}
-  if (req.method == 'POST') {
+  if (req.method === 'POST') {
     req
-    .pipe(limit(1e6, function(){request.connection.destroy()}))
-    .pipe(concat(function(args) {
-      try {
-        args = JSON.parse(args);
-        cssFeatures(args)
-        .pipe(res);
-      } catch(e) {
-        debug('Error processing POST',args,e);
-        res.statusCode = 400;
-        res.end();
-      }
-    }));
-  }
-  // GET /api?..., same args and response as POST /.
-  else if(/^\/api/.test(req.url)) {
-    var args = qs.parse(url.parse(req.url).query);
-    res.setHeader('Content-Type', 'application/json');
-    cssFeatures(args).pipe(res);
-  }
-  else if(/^\/?((\?.*)|$)/.test(req.url)) {
-    var index = trumpet();
+      .pipe(limit(1e6, function () { req.connection.destroy() }))
+      .pipe(concat(function (args) {
+        try {
+          args = JSON.parse(args)
+          cssFeatures(args)
+            .pipe(res)
+        } catch (e) {
+          debug('Error processing POST', args, e)
+          res.statusCode = 400
+          res.end()
+        }
+      }))
+  } else if (/^\/api/.test(req.url)) {
+    // GET /api?..., same args and response as POST /.
+    var args = qs.parse(url.parse(req.url).query)
+    res.setHeader('Content-Type', 'application/json')
+    cssFeatures(args).pipe(res)
+  } else if (/^\/?((\?.*)|$)/.test(req.url)) {
+    var index = trumpet()
     // inline styles to avoid flash of unstyled content.
     fs.createReadStream(__dirname + '/public/main.css')
-      .pipe(index.select('#anti-fouc').createWriteStream());
+      .pipe(index.select('#anti-fouc').createWriteStream())
 
     fs.createReadStream(__dirname + '/public/index.html')
       .pipe(index)
-      .pipe(res);
-  }
-  else {
-    stat(req, res);
+      .pipe(res)
+  } else {
+    stat(req, res)
   }
 })
 
-var port = process.env.PORT || Number(process.argv[2]) || 3000;
-server.listen(port, function() {
-  console.log('doiuse:', 'listening on ',port);
-});
+var port = process.env.PORT || Number(process.argv[2]) || 3000
+server.listen(port, function () {
+  console.log('doiuse:', 'listening on ', port)
+})
